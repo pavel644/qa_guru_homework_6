7a4a4bdcc4a4c3193c9b81204c2bd70dcd6cba55
Pavel Gorozhankin <p.gorozhankin@pravo.tech>
2023-03-06 02:14:54 +0400

    добавлен тест на проверку csv, xlsx, pdf файлов

--- archive_test.py
+++ archive_test.py
@@ -1,6 +1,8 @@
 from zipfile import ZipFile
 import os
 import csv
+import openpyxl
+from PyPDF2 import PdfReader
 
 resources_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources')
 temp_files_dir = os.path.join(resources_dir, 'sample_files')
@@ -16,8 +18,8 @@ def test_csv_file_exist_in_archive():
 def test_csv_file_valid():
     with ZipFile(os.path.join(resources_dir, "sample.zip"), "r") as myzip:
         with open(myzip.extract('sample_csv.csv')) as csv_file:
-            csv_file = csv.reader('sample_csv.csv')
-            assert 'laura@example.com' in str(list(csv_file)[1])
+            csv_file = csv.reader(csv_file)
+            assert 'laura@example.com' in list(csv_file)[1][0]
     os.remove('sample_csv.csv')
 
 
@@ -28,10 +30,22 @@ def test_xlsx_file_exist_in_archive():
 
 def test_xlsx_file_valid():
     with ZipFile(os.path.join(resources_dir, "sample.zip"), "r") as myzip:
-        with open(myzip.extract('sample_xlsx.xlsx')) as xlsx_file:
-
+        myzip.extract('sample_xlsx.xlsx')
+        workbook = openpyxl.load_workbook('sample_xlsx.xlsx')
+        sheet = workbook.active
+        assert sheet.cell(row=4, column=6).value == 'JVF78YQU6HH'
+        os.remove('sample_xlsx.xlsx')
 
 
 def test_pdf_file_exist_in_archive():
     with ZipFile(os.path.join(resources_dir, "sample.zip"), "r") as myzip:
         assert 'sample_pdf.pdf' in myzip.namelist()
+
+
+def test_pdf_file_valid():
+    with ZipFile(os.path.join(resources_dir, "sample.zip"), "r") as myzip:
+        myzip.extract('sample_pdf.pdf')
+        reader = PdfReader('sample_pdf.pdf')
+        page_text = reader.pages[0].extract_text()
+        assert 'ТестёжзфЩ' in page_text
+        os.remove('sample_pdf.pdf')
